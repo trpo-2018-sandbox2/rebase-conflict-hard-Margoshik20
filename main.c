7ee863347972445fcabc2f275a697685d432e630
Сырых Марина Викторовна <ip712s24@mwp05.csc.local>
2018-04-23 08:00:25 +0700

    Two commit

--- main.c
+++ main.c
@@ -31,8 +31,15 @@ int main()
     int arr[] = {3, 1, 4, 1, 5, 9, 2};
 
     const int min = min_element(arr, N_ELEMENTS(arr));
-
-    print_array(arr, N_ELEMENTS(arr));
+    
+    printf("Array: ");
+    int i;
+    for (i = 0; i < size; ++i) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+   
     printf("Min element: %d\n", min);
 
     return 0;
